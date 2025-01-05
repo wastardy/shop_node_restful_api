e2d620d68c3b9230d8ed52e19969d1b5523b6e6a
Andrew Kononenko <wastardy.k@gmail.com>
2025-01-05 15:21:11 +0200

    add logging and error handling

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 const express = require('express');
-// app - екземпляр Express, який використовується 
-// для налаштування маршрутизації, обробників 
-// запитів і middleware.
 
 /*  app - an Express instance used to configure 
     routing, request handlers, and middleware. */
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products.js');
 const orderRoutes = require('./api/routes/orders.js');
-
-// app.use() реєструє middleware, який виконується 
-// для кожного запиту. У цьому випадку будь-який запит 
-// до сервера відповість JSON-об'єктом
 
 /*  app.use() registers the middleware being executed 
     for each request. In this case, any query 
@@ -25,9 +19,32 @@ const orderRoutes = require('./api/routes/orders.js');
     });
 }); */
 
-// anything starter from /products in the url will be 
-// forwarded to ./api/routes/products.js 
+// log incoming requests
+app.use(morgan('dev'));
+
+
+// ===> Routes which should handle requests <===
+/*  anything starter from '/products' in the URL
+    will be forwarded to ./api/routes/products.js */
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// this route will be reached ONLY if previous routes
+// weren't able to handle the request
+app.use((req, res, next) => {
+    const error = new Error('not foud any correct route');
+    error.status = 404;
+    next(error)
+});
+
+// handle any error thrown by the application
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
