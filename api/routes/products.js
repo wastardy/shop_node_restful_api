f61f15e711265a167a5793c25534fa57ca412efb
Andrew Kononenko <wastardy.k@gmail.com>
2025-01-07 15:49:30 +0200

    add CRUD operations for Products model

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,57 +1,116 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/Product.js');
 
 // keep in mind that /products already is at the start of URL
 router.get('/', (req, res, next) => {
-    return res.status(200).json({
-        message: 'handling GET request to /products'
-    });
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+
+            // if (docs.length >= 0) {
+            //     res.status(200).json(docs)
+            // }
+            // else {
+            //     res.status(404).json({
+            //         message: 'No entries found'
+            //     });
+            // }
+
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.get('/:product_id', (req, res, next) => {
     const productId = req.params.product_id;
 
-    if (productId === 'special') {
-        return res.status(200).json({
-            message: 'you discovered the special product ID', 
-            id: productId
+    Product.findById(productId).exec()
+        .then(document => {
+            console.log(`===> from db: ${document}`);
+
+            if (document) {
+                res.status(200).json(document);
+            }
+            else {
+                res.status(404).json({
+                    message: 'No valid entry found for provided ID'
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err})
         });
-    }
-    else {
-        return res.status(200).json({
-            message: 'you passed a product ID'
-        });
-    }
 });
 
 router.post('/', (req, res, next) => {
-    const product = {
-        name: req.body.name,
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name, 
         price: req.body.price
-    };
-
-    return res.status(201).json({
-        message: 'handling POST request to /products',
-        createdProduct: product
     });
+
+    // mongoose method, store my object in DB
+    product.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'handling POST request to /products',
+                createdProduct: result
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.patch('/:product_id', (req, res, next) => {
     const productId = req.params.product_id;
+    const updateOptions = {};
 
-    return res.status(200).json({
-        message: 'update product', 
-        id: productId
-    });
+    for (const option of req.body) {
+        updateOptions[option.prop_name] = option.value;
+    }
+
+    Product.update({ _id: productId }, { $set: updateOptions })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.delete('/:product_id', (req, res, next) => {
     const productId = req.params.product_id;
 
-    return res.status(200).json({
-        message: 'handling DELETE request for specific order', 
-        id: productId
-    });
+    Product.deleteOne({ _id: productId })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 module.exports = router;
