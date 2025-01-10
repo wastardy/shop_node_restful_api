553e20dc750627356be4c35e92a918afc3d08565
Andrew Kononenko <wastardy.k@gmail.com>
2025-01-10 11:40:15 +0200

    add email validations in User schema

--- api/models/User.js
+++ api/models/User.js
@@ -2,8 +2,22 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    email: { type: String, required: true }, 
-    password: { type: String, required: true }
+    email: { 
+        type: String, 
+        required: true,  
+        unique: true, 
+        // stack overflow
+        // "How can I validate an email address in JavaScript?"
+        match: new RegExp(
+            `[a-z0-9!#$%&'*+/=?^_\\\`{|}~-]`
+            + `+(?:\\.[a-z0-9!#$%&'*+/=?^_\\\`{|}~-]+)*@`
+            + `(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)`
+            + `+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?`)
+    }, 
+    password: { 
+        type: String, 
+        required: true 
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
